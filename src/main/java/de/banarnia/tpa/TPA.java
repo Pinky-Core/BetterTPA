b5249ed6f77d500ab810a7b4cb4af4de96c796b1
Mike Nipkow <mail@mike-nipkow.com>
2024-09-23 13:52:39 +0200

    Basic Setup

---
+++ src/main/java/de/banarnia/tpa/TPA.java
@@ -0,0 +1,19 @@
+package de.banarnia.tpa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TPA extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+        // BStats
+
+    }
+}
