783162752a68ba3feeb1c4b044e9150a82c488d4
PinkyGames <147994391+Pinky-Core@users.noreply.github.com>
2025-06-27 18:22:55 -0300

    Tpahere permissions

--- src/main/java/de/banarnia/bettertpa/commands/requests/TPAHereCommand.java
+++ src/main/java/de/banarnia/bettertpa/commands/requests/TPAHereCommand.java
@@ -3,6 +3,7 @@ package de.banarnia.bettertpa.commands.requests;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandCompletion;
+import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.bukkit.contexts.OnlinePlayer;
 import de.banarnia.bettertpa.manager.TPAManager;
@@ -10,6 +11,7 @@ import de.banarnia.bettertpa.requests.TpaHereRequest;
 import org.bukkit.entity.Player;
 
 @CommandAlias("tpahere")
+@CommandPermission("bettertpa.tpahere")
 public class TPAHereCommand extends BaseCommand {
 
     private TPAManager manager;
